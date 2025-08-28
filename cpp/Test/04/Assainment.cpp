0736dbe98bd55ca654b886f5355805e94c04853b
MH TOUFIK <toufik.h@uttarauniversity.edu.bd>
2025-08-28 15:49:59 +0000

    Add initial implementation of brace matching and error reporting in Assainment.cpp and input.txt

---
+++ cpp/Test/04/Assainment.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ifstream myInput("input.txt");
+    ofstream myOutput("output.txt");
+
+    if (!myInput)
+    {
+        cout << "Error: Cannot open input file.\n";
+        return 0;
+    }
+
+    string line;
+    int lineNumber = 1;
+
+    stack<pair<char, int>> braceStack;
+
+    while (getline(myInput, line))
+    {
+        for (char c : line)
+        {
+            if (c == '{')
+            {
+                braceStack.push({'{', lineNumber});
+            }
+            else if (c == '}')
+            {
+                if (!braceStack.empty())
+                {
+                    braceStack.pop();
+                }
+                else
+                {
+                    cout << "Error: closing brace '}' found on line " << lineNumber << ".\n";
+                    myOutput << "Error: closing brace '}' found on line " << lineNumber << ".\n";
+                }
+            }
+        }
+        lineNumber++;
+    }
+
+    while (!braceStack.empty())
+    {
+        auto [brace, lineNum] = braceStack.top();
+        braceStack.pop();
+        cout << "Error: opening brace '{' found on line " << lineNum << ".\n";
+        myOutput << "Error: opening brace '{' found on line " << lineNum << ".\n";
+    }
+
+    myInput.close();
+    myOutput.close();
+
+    return 0;
+}
